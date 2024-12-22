e83638edb2c3a2611b285559ecdf3b6a19482ac1
Amir Dhahri <amirdhahri01@gmail.com>
2024-12-22 01:25:31 +0100

    An update

--- SolidPrinciples/obj/Debug/net9.0/SolidPrinciples.AssemblyInfo.cs
+++ SolidPrinciples/obj/Debug/net9.0/SolidPrinciples.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SolidPrinciples")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+890034a2f9c689cc336df4e8c2512b2cf7751d81")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+50a95da26592f460e36a39120882f076bd239a1d")]
 [assembly: System.Reflection.AssemblyProductAttribute("SolidPrinciples")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SolidPrinciples")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
